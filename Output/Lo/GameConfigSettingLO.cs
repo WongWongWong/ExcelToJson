a6c63539eead31f109d27d8fcbf1f42d8ac144a6
Danny <381558466@qq.com>
2022-03-11 14:09:07 +0800

    示例

---
+++ Output/Lo/GameConfigSettingLO.cs
@@ -0,0 +1,23 @@
+/// <summary> 
+/// 游戏配置
+/// Author:ConfigTool
+/// </summary>
+public class GameConfigSettingLO
+{
+/// <summary> 
+/// 主键
+/// </summary>
+    public int id;
+
+/// <summary> 
+/// 键
+/// </summary>
+    public string key;
+
+/// <summary> 
+/// 值
+/// </summary>
+    public string value;
+
+
+}
